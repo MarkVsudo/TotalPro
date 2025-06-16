fd56eecbb73dda0469c8d1e01e26842496ac58ab
Mark Veskov <mark.veskov@abv.bg>
2025-06-16 13:09:11 +0300

    Setup

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
