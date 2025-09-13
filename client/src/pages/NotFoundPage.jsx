66c2d7eed0fc999ded44a5a327aa233a64e07e4a
Mark Veskov <mark.veskov@abv.bg>
2025-09-14 02:26:47 +0300

    404 page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,23 +1,29 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-          Несъществуваща страница
-        </h1>
-        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
-          Съжаляваме, не успяхме да намерим страницата, която търсите.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6"></div>
-        <a
-          href="/"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Начало
-        </a>
+    <section class="bg-white h-screen flex items-center justify-center">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+          <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-[#002B5B]">
+            404
+          </h1>
+          <p class="mb-4 text-3xl tracking-tight font-bold text-[#002B5B] md:text">
+            Несъществуваща страница
+          </p>
+          <p class="mb-4 text-lg font-light text-gray-500">
+            Съжаляваме, не можем да намерим тази страница. Ще откриете много за
+            разглеждане на началната страница.
+          </p>
+          <Link
+            to="/"
+            class="inline-flex text-[#002B5B] hover:bg-gray-50 border-2 border-[#002B5B] font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4 transition-all"
+          >
+            Начална страница
+          </Link>
+        </div>
       </div>
-    </main>
+    </section>
   );
 };
 
