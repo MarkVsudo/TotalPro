c8f7938255bf98568df2733068d859669cfce3e8
Mark Veskov <veskovmark99@gmail.com>
2025-12-25 01:39:58 +0200

    Sorting functionality implementation

--- client/src/components/AirConPageComponents/AirConProducts.jsx
+++ client/src/components/AirConPageComponents/AirConProducts.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { IoBagAddOutline } from "react-icons/io5";
 import GreeLogo from "../../assets/AirConBrands/gree.png";
@@ -40,6 +41,10 @@ export default function AirConProducts() {
   const [convertedPrices, setConvertedPrices] = useState({});
   const [products, setProducts] = useState([]);
 
+  // Params
+  const [searchParams] = useSearchParams();
+  const sort = searchParams.get("sort") || "Най-популярни";
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -66,9 +71,32 @@ export default function AirConProducts() {
     fetchConversions();
   }, [products]);
 
+  const sortProducts = (products, sortOption) => {
+    switch (sortOption) {
+      case "Най-популярни":
+        return products.sort((a, b) => b.popularity - a.popularity);
+      case "Най-нови":
+        return products.sort(
+          (a, b) => new Date(b.date_added) - new Date(a.date_added)
+        );
+      case "Цена: Възходяща":
+        return products.sort(
+          (a, b) => parseFloat(a.price) - parseFloat(b.price)
+        );
+      case "Цена: Низходяща":
+        return products.sort(
+          (a, b) => parseFloat(b.price) - parseFloat(a.price)
+        );
+      default:
+        return products;
+    }
+  };
+
+  const sortedProducts = sortProducts(products, sort);
+
   return (
     <div className="bg-white grid gap-x-6 xl:gap-x-8 gap-y-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-      {products.map((product, index) => (
+      {sortedProducts.map((product, index) => (
         <Link to={product.href} key={index}>
           <div className="group relative h-full flex flex-col">
             <div className="relative overflow-hidden ">
