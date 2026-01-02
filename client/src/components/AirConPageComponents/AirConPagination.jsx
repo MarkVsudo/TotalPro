b49abf050b8f94b31d9f5d7e771681710db1a876
Mark Veskov <veskovmark99@gmail.com>
2026-01-02 22:43:21 +0200

    Implemented pagination logic

--- client/src/components/AirConPageComponents/AirConPagination.jsx
+++ client/src/components/AirConPageComponents/AirConPagination.jsx
@@ -1,28 +1,26 @@
+import { useSearchParams } from "react-router-dom";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowBack } from "react-icons/io";
 
-export default function AirConPagination() {
+export default function AirConPagination({ total }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page")) || 1;
+
+  const updatePage = (page) => {
+    setSearchParams((prev) => {
+      prev.set("page", page);
+      return prev;
+    });
+  };
+
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white py-3 mt-4">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Previous
-        </a>
-        <a
-          href="#"
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-        >
-          Next
-        </a>
-      </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
           <p className="text-sm text-gray-700">
             Показани са <span className="font-medium">1-12</span> от{" "}
-            <span className="font-medium">97</span> резултата
+            <span className="font-medium">{total}</span> резултата
           </p>
         </div>
         <div>
@@ -30,61 +28,42 @@ export default function AirConPagination() {
             aria-label="Pagination"
             className="isolate inline-flex -space-x-px rounded-md shadow-xs"
           >
-            <a
-              href="#"
+            <button
+              disabled={page === 1}
+              onClick={() => updatePage(page - 1)}
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
-              <span className="sr-only">Previous</span>
               <IoIosArrowBack aria-hidden="true" className="size-5" />
-            </a>
-            {/* Current: "z-10 bg-blue-600 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center bg-[#002B5B] px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#002B5B]"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              2
-            </a>
-            <a
-              href="#"
-              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-            >
-              3
-            </a>
-            <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-gray-300 ring-inset focus:outline-offset-0">
+            </button>
+
+            {Array.from({ length: Math.ceil(Number(total) / 12) }).map(
+              (_, index) => (
+                <button
+                  type="button"
+                  key={index}
+                  onClick={() => updatePage(index + 1)}
+                  className={`relative inline-flex items-center px-4 py-2 text-sm font-semibold focus:z-20 ${
+                    page === index + 1
+                      ? "z-10 bg-[#002B5B] text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#002B5B]"
+                      : "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
+                  }`}
+                >
+                  {index + 1}
+                </button>
+              )
+            )}
+
+            {/* <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-gray-300 ring-inset focus:outline-offset-0">
               ...
-            </span>
-            <a
-              href="#"
-              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-            >
-              8
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              9
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-            >
-              10
-            </a>
-            <a
-              href="#"
+            </span> */}
+
+            <button
+              disabled={page === Math.ceil(Number(total) / 12)}
+              onClick={() => updatePage(page + 1)}
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
-              <span className="sr-only">Next</span>
               <IoIosArrowForward aria-hidden="true" className="size-5" />
-            </a>
+            </button>
           </nav>
         </div>
       </div>
