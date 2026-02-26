a427bffb65dd79374c1e596b558c1142673502c9
Mark Veskov <veskovmark99@gmail.com>
2026-02-26 12:20:32 +0200

    Added trust proxy

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
+app.set("trust proxy", 1);
 app.get("/", (req, res) => {
   res.send("API is running ✅");
 });
